cf21d405c6e849d50f20cd8f6ce9ae2215144d4a
Matyanson <matya.peremsky@gmail.com>
2020-10-24 12:40:47 +0200

    user context component

---
+++ components/UserContext.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useContext } from 'react';
+import nookies from 'nookies'
+import firebase from 'firebase/app';
+
+// ...
+
+const AuthContext = React.createContext<{ user: firebase.User | null }>({
+  user: null,
+});
+
+export default function AuthProvider({ children, token }: any) {
+  const [user, setUser] = useState<firebase.User | null>(token);
+
+  useEffect(() => {
+    return firebase.auth().onIdTokenChanged(async (user) => {
+      if (!user) {
+        setUser(null);
+        nookies.set(undefined, 'token', '', {});
+        return;
+      }
+      
+      const token = await user.getIdToken();
+      setUser(user);
+      nookies.set(undefined, 'token', token, {});
+    });
+  }, []);
+  //console.log(token);
+  return (
+    <AuthContext.Provider value={{ user }}>{
+      children
+    }</AuthContext.Provider>
+  );
+}
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
