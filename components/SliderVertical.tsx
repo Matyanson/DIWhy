b30677b68362ffaf661203d44d88e8898775ecf6
Matyanson <matya.peremsky@gmail.com>
2020-12-30 13:46:50 +0100

    fix(VerticalSlider): passing new value from parent

---
+++ components/SliderVertical.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import { useTheme } from "./ThemeProvider";
+
+interface Props{
+    max?: number,
+    min?: number,
+    value?: number,
+    inverted?: boolean,
+    onChange?: (n:number)=>void
+}
+
+const SliderVertical = ({
+    max = 100,
+    min = 0,
+    value = 0,
+    inverted = false,
+    onChange = ()=>{}
+}: Props)=> {
+    const [theme] = useTheme();
+    const [innerValue, setValue] = useState(value);
+    const percentage = useMemo(() => { return innerValue / (max / 100) }, [innerValue]);
+
+    useEffect(()=>{
+        setValue(value);
+    }, [value])
+
+    function updateValue(newValue){
+        const res = inverted ?
+        max - newValue :
+        newValue;
+        setValue(res);
+        onChange(res);
+    }
+    return (
+        <div className="wrap">
+            <input type="range" min={min} max={max} value={value} onChange={(e)=>updateValue(e.target.value)}></input>
+            <div className={`slider ${inverted?'inverted': ''}`}>
+                <div className="track"></div>
+                <div className="range" style={{height: `${percentage}%`}} ></div>
+                <div className="thumb" style={inverted ? {top: `${percentage}%`, transform: `translateY(-50%)`} : {bottom: `${percentage}%`}} ></div>
+            </div>
+            <style jsx>{`
+                .wrap{
+                    position: relative;
+                    height: 100%;
+                    width: 20px;
+                }
+                input[type=range]{
+                    z-index: 2;
+                    position: absolute;
+                    top:0;
+                    left:0;
+                    -webkit-appearance: slider-vertical;
+                    height: 100%;
+                    width: 100%;
+                    opacity: 0;
+                }
+                .slider{
+                    z-index: 1;
+                    position: relative;
+                    height: 100%;
+                    width: 100%;
+                }
+                .slider > .track{
+                    background: #000;
+                    width:2px;
+                    height: 100%;
+                    margin: 0 auto;
+                    border-radius: 5px;
+                }
+                .slider > .range{
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    margin: 0 auto;
+                    background: ${theme.dark};
+                    width: 5px;
+                    border-radius: 5px;
+                    transition: all 0.1s;
+                }
+                .slider > .thumb{
+                    position: absolute;
+                    transform: translateY(50%);
+                    width: 10px;
+                    height: 10px;
+                    left: 0;
+                    right: 0;
+                    margin: 0 auto;
+                    border-radius: 50px;
+                    background: ${theme.dark};
+                    opacity: 1;
+                    transition: all 0.1s;
+                }
+                .thumb:hover{
+                    opacity: 1;
+                }
+                .slider.inverted > .range{
+                    top: 0;
+                    left: 0;
+                }
+                
+            `}</style>
+        </div>
+    );
+}
+
+export default SliderVertical;
