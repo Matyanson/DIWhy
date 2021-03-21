5f59d8ef7b038819e517940a0f0d1875f2afe7b1
Matyanson <matya.peremsky@gmail.com>
2021-03-21 23:43:38 +0100

    feat(Navbar): mobile responsivity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,17 @@ import { useAuth } from './UserProvider';
 import SchemeSwitch from './SchemeSwitch';
 import { Router, useRouter } from 'next/router';
 import ProfilePic from './ProfilePicture';
+import { useState } from 'react';
+import { Menu } from './icons';
 
 const Navbar = ()=>{
     const user = useAuth();
     const router = useRouter();
     const [ { background, nav_BG, nav_FG, text, primary, neutral, neutral_dark} ] = useTheme();
+    const [expand, setExpand] = useState(true);
     return (
         <div>
-            <ul className="nav">
+            <ul className={`nav ${expand ? 'expand' : ''}`}>
                 <li className={ router.pathname == "/" ? "active" : ""}>
                     <Link href="/"><a>Home</a></Link>
                 </li>
@@ -48,6 +51,9 @@ const Navbar = ()=>{
                     <SchemeSwitch />
                 </li>
             </ul>
+            <div id="mobileHeader">
+                <Menu onClick={()=>setExpand(!expand)}/>
+            </div>
             
             <style jsx >{`
                 .nav{
@@ -65,6 +71,9 @@ const Navbar = ()=>{
                     align-items: stretch;
                     justify-content: left;
                     z-index:100;
+                }
+                #mobileHeader{
+                    display: none;
                 }
                 ul.nav{
                     text-decoration: none;
@@ -97,6 +106,50 @@ const Navbar = ()=>{
                 }
                 .active a{
                     color: ${primary}!important;
+                }
+                @media only screen and (max-width: 1024px) {
+                    .nav{
+                        top: inherit;
+                        bottom:0;
+                        margin-bottom: 40px;
+                        flex-flow: column;
+                        align-items: center;
+                        height: 20px;
+                        overflow: hidden;
+                        height:0;
+                    }
+                    .nav.expand{
+                        height: auto;
+                        padding-bottom: 10px;
+                    }
+                    #mobileHeader{
+                        display: flex;
+                        align-items: center;
+                        justify-content: flex-end;
+                        flex-flow: row;
+                        position: fixed;
+                        bottom: 0;
+                        left:0;
+                        background: ${nav_BG};
+                        box-shadow: 0px 2px 5px 0px #0000004f;
+                        padding: 10px;
+                        color: ${nav_FG};
+                        font-size: 1.5rem;
+                        width: 100%;
+                        height: 40px;
+                        z-index: 101;
+                    }
+                    .nav li{
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                    }
+                    .nav li a{
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                        padding: 5px;
+                    }
                 }
             `}</style>
         </div>
