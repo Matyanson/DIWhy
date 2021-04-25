be2d49cc00f4c325d19e74e8b47627c0fb59c49e
Matyanson <matya.peremsky@gmail.com>
2021-04-25 18:35:28 +0200

    fix(): PWA fix2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,37 @@
-import Document from 'next/document'
-import Head from 'next/head'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
 
   render(){
     return(
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>DIWHY PWA App</title>
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>DIWHY PWA App</title>
 
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="logo.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link rel="apple-touch-icon" href="logo-apple.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="logo.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link rel="apple-touch-icon" href="logo-apple.png"></link>
+          <meta name="theme-color" content="#317EFB" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     )
   }
   static async getInitialProps(ctx) {
