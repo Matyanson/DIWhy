7328813ed6c7d08aa90837a15606af3d8ae865e3
Matyanson <matya.peremsky@gmail.com>
2021-04-25 15:12:11 +0200

    feat(): PWA support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Layout from '../components/Layout';
 import '../styles/globals.css';
 import { useEffect, useState } from 'react';
 import ColorTemplate from '../models/ColorTemplate';
+import Head from 'next/head';
 
 
 
@@ -25,7 +26,29 @@ function MyApp({ Component, pageProps, user, userData}) {
   // console.log("userData _app", userData);
   // console.log("user _app", user);
   return (
-    
+    <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <meta name="description" content="Description" />
+      <meta name="keywords" content="Keywords" />
+      <title>DIWHY PWA App</title>
+
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        href="logo.png"
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link rel="apple-touch-icon" href="logo-apple.png"></link>
+      <meta name="theme-color" content="#317EFB" />
+    </Head>
+
     <AuthProvider initialUser={user} initialUserData={userData}>
       <ThemeProvider initialTheme={ initTheme }>
           <Layout>
@@ -33,6 +56,7 @@ function MyApp({ Component, pageProps, user, userData}) {
           </Layout>
       </ThemeProvider>
     </AuthProvider>
+    </>
   )
   
 }
---
+++ pages/_offline.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+
+
+
+
+const Offline = ()=> {
+        
+  return (
+    <div>
+        <Head>
+            <title>Offline</title>
+        </Head>
+        <h1>Offline</h1>
+    </div>
+  );
+}
+
+export default Offline;
